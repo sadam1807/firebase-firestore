a867e780311966cb769d24f53a5256b670fe5dc0
sadam1807 <sadam.bapunawar@gmail.com>
2018-12-13 16:05:05 +0530

    fetche data from firebase

---
+++ app.js
@@ -0,0 +1,21 @@
+const cafeList = document.querySelector('#cafe-list');
+
+// create element on render cafe
+
+function renderCafe(doc){
+   let li = document.createElement('li');
+   let name = document.createElement('span');
+   let city = document.createElement('span');
+   li.setAttribute('date-id', doc.id);
+   name.textContent = doc.data().name;
+   city.textContent = doc.data().city;
+   li.appendChild(name);
+   li.appendChild(city);
+   cafeList.appendChild(li);
+}
+
+db.collection('cafes').get().then((snapshot) => {
+     snapshot.docs.forEach(doc => {
+         renderCafe(doc);
+     })
+})
